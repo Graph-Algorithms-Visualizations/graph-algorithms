c1c5dfe933556907fc51a2bcbcb1da27fff07fdb
siddharthlal25 <siddharthlal25@gmail.com>
2019-09-05 19:02:03 +0530

    node removing function added

--- test.py
+++ test.py
@@ -18,7 +18,34 @@ dict1 = {
     2 : [6,7]
 }
 
+edge_matrix = [[None,None,1],
+        [None,None,1],
+        [1,1,None]]
+
+
+#
+
 node = {}
 # Creates a dictionary of Node Objects as value linked to their indices as keys
 for i in dict:
     node[i] = Node(dict[i][0],dict[i][1])
+
+
+# edge_matrix is the matrix consisting of connections
+# Sample edge_matrix:
+# edge_matrix = [[None,None,1],
+#         [None,None,1],
+#         [1,1,None]]
+# node is dictionary of Node Objects as value linked to their indices as keys
+# Sample node dictionary for testing:
+# {0: [1, 2], 1: [4, 5], 2: [6, 7]}
+
+def remove_connections(index):
+    for i in range(len(edge_matrix)):
+        edge_matrix[index][i] = None
+        edge_matrix[i][index] = None
+
+def remove_node(index):
+    del node[index]
+    remove_connections(index)
+
