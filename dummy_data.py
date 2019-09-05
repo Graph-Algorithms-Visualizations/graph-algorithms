97373295b5e4eef70bfbc4c10a13830e08cfaf36
siddharthlal25 <siddharthlal25@gmail.com>
2019-09-05 19:17:23 +0530

    Removed test.py and merged into dummy_data.py

--- dummy_data.py
+++ dummy_data.py
@@ -1,5 +1,17 @@
 from graphic_items import Node, Edge
 
+class Vertex:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+class Linkage:
+    def __init__(self, end1, end2, weight = 0):
+        self.ends = []
+        self.ends.append(end1)
+        self.ends.append(end2)
+        self.weight = weight
+
 matrix = [
     [0, 1, 0, 0],
     [0, 0, 1, 0],
@@ -14,6 +26,13 @@ node_objs = {
     3: Node(100, 50, 3)
 }
 
+# node_objs = {}
+# # Creates a dictionary of Node Objects as value linked to their indices as keys
+# def node_object_convert(dict1)
+# for i in dict:
+#     node_objs[i] = Vertex(dict[i][0],dict[i][1])
+
+
 edge_matrix = []
 
 for i in range(len(matrix)):
@@ -24,6 +43,15 @@ for i in range(len(matrix)):
         else:
             row.append(Edge(node_objs[i], node_objs[j]))
     edge_matrix.append(row)
+
+def remove_connections(index):
+    for i in range(len(edge_matrix)):
+        edge_matrix[index][i] = None
+        edge_matrix[i][index] = None
+
+def remove_node(index):
+    del node_objs[index]
+    remove_connections(index)
 
 
 def get_processed_data():
