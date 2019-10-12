e8d81713a04dc5d2fc5963f5a4526f36764f8429
Arib Alam <aribalam64@gmail.com>
2019-10-13 00:44:19 +0530

    added color attribute to backend data

--- data_parser.py
+++ data_parser.py
@@ -1,17 +1,18 @@
 from graphic_items import Node, Edge
-
+from PyQt5.QtGui import QColor
 
 def processFrontendData(node_objs, edge_matrix):
 
     node_data = {
         key: {
             'x': val.center.x(),
-            'y': val.center.y()
+            'y': val.center.y(),
+            'color': (val.color.red(), val.color.green(), val.color.blue())
         }
         for key, val in node_objs.items()
     }
 
-    edge_data = [[1 if edge else 0 for edge in row] for row in edge_matrix]
+    edge_data = [[{'color': (edge.color.red(), edge.color.green(), edge.color.blue())} if edge else None for edge in row] for row in edge_matrix]
     return {
         'nodes': node_data,
         'edges': edge_data
@@ -21,7 +22,7 @@ def processFrontendData(node_objs, edge_matrix):
 def processBackendData(node_data, edge_data):
 
     node_objs = {
-        key: Node(val['x'], val['y'], key)
+        key: Node(val['x'], val['y'], key, QColor(val['color'][0], val['color'][1], val['color'][2]))
         for key, val in node_data.items()
     }
 
@@ -30,7 +31,8 @@ def processBackendData(node_data, edge_data):
         row = []
         for j in range(len(edge_data)):
             if edge_data[i][j]:
-                row.append(Edge(node_objs[i], node_objs[j]))
+                row.append(Edge(node_objs[i], node_objs[j], QColor(edge_data[i][j]['color'][0], edge_data[i][j]['color'][1],
+                                                                   edge_data[i][j]['color'][2])))
             else:
                 row.append(None)
         edge_matrix.append(row)
